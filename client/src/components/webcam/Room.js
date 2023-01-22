52ddb957f071224f1927789bd73e51cd4257d263
Jonathan Adithya <jo.adithya391@gmail.com>
2023-01-22 13:25:01 -0800

    Add link to the back button for the conference

--- client/src/components/webcam/Room.js
+++ client/src/components/webcam/Room.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Button } from '../form/Button';
 import { WebcamComponent } from './WebcamComponent';
 import { MeetingDescription } from './MeetingDescription';
@@ -6,9 +7,11 @@ import { MeetingDescription } from './MeetingDescription';
 const Header = ({ title }) => {
   return (
     <div className="w-full border-slate-300 border-b-[1px] flex items-center py-4 gap-5">
-      <Button type="button" className="w-10 h-10" muted>
-        {'<'}
-      </Button>
+      <Link to="/">
+        <Button type="button" className="w-10 h-10" muted>
+          {'<'}
+        </Button>
+      </Link>
       <h1 className="font-semibold tracking-[0.44px] text-text text-2xl">
         SyncUp Conference
       </h1>
