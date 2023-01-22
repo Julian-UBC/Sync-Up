b9a4b4046052499e153420eab261dda797f32201
Julian <julian.widjaja02@gmail.com>
2023-01-22 11:09:59 -0800

    Fix profile section in sidebar

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,7 +1,7 @@
-import { useState, useRef, useEffect } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import Avatar from '@mui/material/Avatar';
-import { deepOrange } from '@mui/material/colors';
+import { useState, useRef, useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
+import { deepOrange } from "@mui/material/colors";
 
 import {
   HomeOutlined,
@@ -9,9 +9,9 @@ import {
   GroupsOutlined,
   SettingsOutlined,
   MenuBookOutlined,
-} from '@mui/icons-material';
+} from "@mui/icons-material";
 
-import './Sidebar.css';
+import "./Sidebar.css";
 
 let useClickInside = (handler) => {
   let domNode = useRef();
@@ -22,9 +22,9 @@ let useClickInside = (handler) => {
         handler();
       }
     };
-    document.addEventListener('mousedown', maybeHandler);
+    document.addEventListener("mousedown", maybeHandler);
     return () => {
-      document.removeEventListener('mousedown', maybeHandler);
+      document.removeEventListener("mousedown", maybeHandler);
     };
   });
 
@@ -37,12 +37,12 @@ export const Sidebar = ({ children }) => {
 
   const routes = [
     {
-      path: '/',
-      title: 'Home',
+      path: "/",
+      title: "Home",
       icon: <HomeOutlined />,
     },
-    { path: 'schedule', title: 'Schedule', icon: <CalendarMonthOutlined /> },
-    { path: 'meeting', title: 'Meeting', icon: <GroupsOutlined /> },
+    { path: "schedule", title: "Schedule", icon: <CalendarMonthOutlined /> },
+    { path: "meeting", title: "Meeting", icon: <GroupsOutlined /> },
   ];
 
   let domNode = useClickInside(() => {
@@ -54,28 +54,37 @@ export const Sidebar = ({ children }) => {
   };
 
   useEffect(() => {
-    console.log(160 + page * 52)
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.setProperty('--y-pos', 160 + page * 52 + 'px');
+    console.log(160 + page * 52);
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.setProperty("--y-pos", 160 + page * 52 + "px");
   }, [page]);
 
   const handleClick = (idx) => {
-    console.log('click', idx)
+    console.log("click", idx);
     setPage(idx);
-  }
+  };
 
   return (
     <div className="flex">
       <div
         className={` ${
-          isOpen ? 'translate-x-0' : 'translate-x-[-185px]'
+          isOpen ? "translate-x-0" : "translate-x-[-185px]"
         } ease-in-out w-[265px] bg-main text-white h-screen p-4 pt-8 duration-[.4s] fixed sidebar`}
         ref={domNode}
         onMouseEnter={handleMouseEnterSidebar}
       >
-        <Link to="/" className={`${isOpen ? 'translate-x-0' : 'translate-x-[185px]'} ease-in-out transition-transform duration-[.4s] flex gap-x-4 items-center p-2 text-2xl`}>
+        <Link
+          to="/"
+          className={`${
+            isOpen ? "translate-x-0" : "translate-x-[185px]"
+          } ease-in-out transition-transform duration-[.4s] flex gap-x-4 items-center p-2 text-2xl`}
+        >
           <MenuBookOutlined className="" />
-          <h1 className={`${!isOpen ? 'scale-x-0 opacity-0' : 'scale-x-100 opcaity-100'} ease-in-out transition origin-left duration-[.4s] font-medium`}>
+          <h1
+            className={`${
+              !isOpen ? "scale-x-0 opacity-0" : "scale-x-100 opcaity-100"
+            } ease-in-out transition origin-left duration-[.4s] font-medium`}
+          >
             Sync Up
           </h1>
         </Link>
@@ -86,12 +95,18 @@ export const Sidebar = ({ children }) => {
                 to={item.path}
                 key={index}
                 onClick={() => handleClick(index)}
-                className={`${isOpen ? 'translate-x-0' : 'translate-x-[185px]'} ease-in-out transition-transform duration-[.4s] flex rounded-md p-2 cursor-pointer text-white hover:bg-white hover:text-secondary text-md items-center gap-x-4 
-              ${item.gap ? 'mt-9' : 'mt-2'} ${index === 0 && 'bg-light-white'} ${(navData) => (navData.isActive ? "active-style" : 'none')}`}
+                className={`${
+                  isOpen ? "translate-x-0" : "translate-x-[185px]"
+                } ease-in-out transition-transform duration-[.4s] flex rounded-md p-2 cursor-pointer text-white hover:bg-white hover:text-secondary text-md items-center gap-x-4 
+              ${item.gap ? "mt-9" : "mt-2"} ${
+                  index === 0 && "bg-light-white"
+                } ${(navData) => (navData.isActive ? "active-style" : "none")}`}
               >
                 {item.icon}
                 <span
-                  className={`${!isOpen ? 'scale-x-0 opacity-0' : 'scale-x-100 opacity-100'} ease-in-out origin-left transition duration-[.2s]`}
+                  className={`${
+                    !isOpen ? "scale-x-0 opacity-0" : "scale-x-100 opacity-100"
+                  } ease-in-out origin-left transition duration-[.2s]`}
                 >
                   {item.title}
                 </span>
@@ -101,20 +116,41 @@ export const Sidebar = ({ children }) => {
         </div>
         <NavLink
           to="/profile"
-          className={`${isOpen ? 'translate-x-0' : 'translate-x-[185px]'} ease-in-out transition-transform duration-[.4s] absolute bottom-0 w-full left-0 flex rounded-md p-2 cursor-pointer text-white hover:bg-white hover:text-secondary text-md items-center gap-3 text-sm w-[calc(100%-2rem)] left-[1rem] bottom-[1rem]`}
+          className={`${
+            isOpen ? "translate-x-0" : "translate-x-[185px]"
+          } ease-in-out transition-transform duration-[.4s] absolute flex rounded-md p-2 cursor-pointer text-white text-md items-center gap-3 text-sm w-[calc(100%-2rem)] left-[1rem] bottom-[1rem]`}
           onClick={() => handleClick(-4)}
         >
-          <Avatar sx={{ bgcolor: deepOrange[500], fontSize: '16px', width: 36, height: 36 }}>JW</Avatar>
-          <div className={`${!isOpen ? 'scale-x-0 opacity-0' : 'scale-x-100 opacity-100'} ease-in-out flex transition h-max-[36px] origin-left duration-[.4s] items-center justify-between w-full`}>
+          <Avatar
+            sx={{
+              bgcolor: deepOrange[500],
+              fontSize: "16px",
+              width: 36,
+              height: 36,
+            }}
+          >
+            JW
+          </Avatar>
+          <div
+            className={`${
+              !isOpen ? "scale-x-0 opacity-0" : "scale-x-100 opacity-100"
+            } ease-in-out flex transition h-max-[36px] origin-left duration-[.4s] items-center justify-between w-full`}
+          >
             <div className="flex flex-col items-start w-max">
               <h3 className="font-semibold">Julian Widjaja</h3>
-              <p className="text-xs">View Profile</p>
+              <p className="text-xs hover:text-secondary">View Profile</p>
             </div>
-            <SettingsOutlined />
+            <SettingsOutlined className="hover:text-secondary" />
           </div>
         </NavLink>
       </div>
-      <main className={`${isOpen ? 'pl-[265px]' : 'pl-20'} transition-[padding] ease-in-out duration-[.4s] w-full bg-[#e9f1f1] min-h-[100vh]`}>{children}</main>
+      <main
+        className={`${
+          isOpen ? "pl-[265px]" : "pl-20"
+        } transition-[padding] ease-in-out duration-[.4s] w-full bg-[#e9f1f1] min-h-[100vh]`}
+      >
+        {children}
+      </main>
     </div>
   );
 };
