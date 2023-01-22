f9fe0d9ff101ceeaa289e649e506702a2a6689e1
Jonathan Adithya <jo.adithya391@gmail.com>
2023-01-22 00:03:03 -0800

    Add route for profile settings page

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,16 @@ import {
   Routes,
   Navigate,
 } from "react-router-dom";
-import { Dashboard } from "./main/Dashboard";
-import { WebcamPage } from "./pages/webcam/WebcamPage";
-
-import "./App.css";
 
 import { Dashboard } from "./pages/Dashboard";
+import { WebcamPage } from "./pages/webcam/WebcamPage";
 import { Sidebar } from "./components/Sidebar";
 import { Schedule } from "./pages/Schedule";
 import { Meeting } from "./pages/Meeting";
 import { Settings } from "./pages/Settings";
+import { ProfileSettings } from "./pages/ProfileSettings";
+
+import "./App.css";
 
 function App() {
   return (
@@ -28,6 +28,7 @@ function App() {
             <Route exact path="meeting" element={<Meeting />} />
             <Route exact path="webcam" element={<WebcamPage />} />
             <Route exact path="settings" element={<Settings />} />
+            <Route exact path="profile" element={<ProfileSettings />} />
             <Route path="*" element={<Navigate replace to="/" />} />
           </Routes>
         </Sidebar>
