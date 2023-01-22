e13e7711fdc2deee39db3f632306356049e9af47
Jonathan Adithya <jo.adithya391@gmail.com>
2023-01-21 17:39:44 -0800

    Create a new Room component

---
+++ client/src/components/webcam/Room.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button } from '../form/Button';
+import { WebcamComponent } from './WebcamComponent';
+import { MeetingDescription } from './MeetingDescription';
+
+const Header = ({ title }) => {
+  return (
+    <div className="w-full border-slate-300 border-b-[1px] flex items-center py-4 gap-5">
+      <Button type="button" className="w-10 h-10" muted>
+        {'<'}
+      </Button>
+      <h1 className="font-semibold tracking-[0.44px] text-text text-2xl">
+        SyncUp Conference
+      </h1>
+    </div>
+  );
+};
+
+export const Room = ({ title, description }) => {
+  return (
+    <div className="w-full h-full p-4 flex gap-4">
+      <div className="w-full h-full flex flex-col gap-6">
+        <Header title={title} />
+        <WebcamComponent />
+      </div>
+      <MeetingDescription title={title} description={description} />
+    </div>
+  );
+};
